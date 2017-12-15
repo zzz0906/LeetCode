b4d45ad64b8eb1939fe2718cd4a8f3c3bfd55359
zzz0906 <zzz879978@outlook.com>
2017-12-15 20:20:06 +0800

    for 27

--- 27.cpp
+++ 27.cpp
@@ -4,11 +4,11 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
     	int all = nums.size();
-  		for (vector<int>::iterator it = nums.begin(); it != nums.end();it++){
+  		for (vector<int>::iterator it = nums.begin(); it != nums.end();){
   			if (*it == val) {
 			  it = nums.erase(it);
 			  all--;
-			}
+			}else ++it;
 		}
 		return all;
     }
