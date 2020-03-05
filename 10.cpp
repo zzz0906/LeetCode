b56cde57a8504c6b22394f0ed3ae5bf9aec9c7d7
Zhongzhu Zhou <charles@Zhongzhus-MacBook-Pro.local>
2020-03-05 14:42:11 +0800

    fail to finish 10 by myself TT

--- 10.cpp
+++ 10.cpp
@@ -5,12 +5,12 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         bool if_there_is_st = false, if_there_is_dot = false;
-        for (int i = 0; i <s.length();i++){
-            if (s[i] == '*'){
+        for (int i = 0; i <p.length();i++){
+            if (p[i] == '*'){
                 if_there_is_st = true;
                 break;
             }
-            if (s[i] == '.'){ 
+            if (p[i] == '.'){ 
                 if_there_is_dot = true;
                 break;
             }
@@ -19,10 +19,42 @@ public:
             if (s != p)
                 return false;
         }
+        char precd;
+        int pointtos = 0;
+        for (int i = 0;i<p.length();i++){
+            if (p[i] == '*'){
+                if (precd == '.') {
+                    precd = s[pointtos];
+                }
+                while (pointtos < s.length() && s[pointtos] == precd)
+                    pointtos++;
+                continue;
+            }
+            if (p[i] != '*' && p[i] != '.'){
+                if (i + 1 < p.length() && p[i + 1] == '*'){
+                    precd = p[i];
+                    continue;}
+                if (p[i] == s[pointtos])
+                    pointtos++;
+                else
+                    return false;
+                continue;
+            }
+            precd = p[i];
+            if (pointtos == s.length() && i < p.length() - 1){
+                i = i + 1;
+                while (i < p.length())
+                    if (p[i] == '.' && p[i] != '&')
+                        return false;
+                return true;
+            }
+        }
+        if (pointtos < s.length() && p[p.length() - 1] != '*')
+            return false;
         return true;
     }
 };
 int main(){
     Solution s;
-    cout<<s.isMatch("aa","a")<<endl;
+    cout<<s.isMatch("aab","c*a*b")<<endl;
 }
--- README.md
+++ README.md
@@ -4,7 +4,31 @@ Practicing my programming skill day by day
 Record the consideration and analysis
 
 ## 10 Regular Expression Matching
-Because it just contains '*', '.'.
-I think we can detect if there are these two symbols in the pattern string. If there are not these two symbols we can just return whether these two strings are eaqual or not.
+~~Because it just contains '*', '.'.
+I think we can detect if there are these two symbols in the pattern string. If there are not these two symbols we can just return whether these two strings are eaqual or not.~~
 
-If there are '*', 
+~~We use a **pointer** to point the position in s we want to compare. We use a **proced** variable to record the previous value for '*'.~~
+
+~~If there is '*', that means we can fit a character with any times; then we repeat the pointer ++ until we cannot find the same one.~~
+
+~~If there is '.', that means we can fit a random character. we can just add pointer;~~
+
+~~If there is a albert, we need to compare it.~~
+
+Then we can program the code in 10.cpp.
+
+However
+"aab"
+"c*a*b"
+
+Output
+false
+
+Expected
+true
+
+1. ~~We need to skip if we detect one character~~
+2. ~~if the next char is '*' we need to skip too~~
+3. ~~If we find all pattern but pointtos < s.length return false~~
+
+Oh oh, it's not that easy; It's a **Recursion** or **Dynamic Programming** exercise. I refer to the **solution**:
