0e532b396f4308bbcfcb16382d0544b2a50a7dc5
zzz0906 <zzz879978@outlook.com>
2020-07-30 12:20:20 +0800

    finish 137

---
+++ 137_std.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int one = 0, two = 0, three = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            two |= one & nums[i];
+            one ^= nums[i];
+            three = one & two;
+            one &= ~three;
+            two &= ~three;
+        }
+        return one;
+    }
+};
