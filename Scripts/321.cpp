cea41139ecf49fc4c6bc98217c0c15770ffd435f
zzz0906 <zzz879978@outlook.com>
2021-03-15 18:28:24 +0800

    finish 14 in March 2021 and update 321

--- Scripts/321.cpp
+++ Scripts/321.cpp
@@ -1,3 +1,6 @@
+#include <vector> 
+#include <iostream>
+using namespace std;
 class Solution {
 public:
     vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
@@ -22,18 +25,56 @@ public:
         while (answer.size() < k){
             int st = 9;
             while (true){
+                int max1 = -1;
+                int pos1 = 0;
                 if (records_1[st].size() != 0){
-                    for (int i = 0; i < records_1[st].size(); i++)
+                    for (int i = 0; i < records_1[st].size(); i++){
                         if (records_1[st][i] >= nums1.size() - rest1 && nums1.size() - records_1[st][i] - 1 + rest2 >= k - answer.size()){
-                        rest1 = nums1.size() - records_1[st][i] - 1;
-                        records_1[st].erase(records_1[st].begin(),records_1[st].begin() + i);
-                        answer.push_back(st);
+                        
+                        //rest1 = nums1.size() - records_1[st][i] - 1;
+                        //records_1[st].erase(records_1[st].begin(),records_1[st].begin() + i);
+                        //answer.push_back(st);
+                        max1 = st;
+                        pos1 = i;
+                        break;
+                        }
+                    }
+                }
+                int max2 = -1;
+                int pos2 = 0;
+                if (records_2[st].size() != 0){
+                    for (int i = 0; i < records_2[st].size(); i++)
+                        if (records_2[st][i] >= nums2.size() - rest2 && nums2.size() - records_2[st][i] - 1 + rest1 >= k - answer.size()){
+                        //rest1 = nums1.size() - records_1[st][i] - 1;
+                        //records_1[st].erase(records_1[st].begin(),records_1[st].begin() + i);
+                        //answer.push_back(st);
+                        max2 = st;
+                        pos2 = i;
                         break;
                         }
                 }
-                
+                if (max2 != -1 || max1 != -1){
+                    if (max2 > max1){
+                        rest2 = nums2.size() - records_2[max2][pos2] - 1;
+                        records_2[max2].erase(records_2[max2].begin(),records_2[max2].begin() + pos2);
+                        answer.push_back(max2);
+                    }else{
+                        rest1 = nums1.size() - records_1[max1][pos1] - 1;
+                        records_1[max1].erase(records_1[max1].begin(),records_1[max1].begin() + pos1);
+                        answer.push_back(max1);
+                    }
+                    break;
+                }
                 st --;
             }
         }
+        return answer;
     }
 };
+
+int main(){
+    vector<int> v1; v1.push_back(3);v1.push_back(4);v1.push_back(6);v1.push_back(5);
+    vector<int> v2; v2.push_back(9);v2.push_back(2);v2.push_back(5);v2.push_back(8);v2.push_back(3);
+    Solution s;
+    s.maxNumber(v1,v2,3);
+}
